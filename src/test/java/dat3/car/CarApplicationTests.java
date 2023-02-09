c1f15554cb81bb653c8ca03088167cf45354a779
Nicolai Andersen <niiicolai@gmail.com>
2023-02-09 23:12:18 +0100

    remove context test

--- src/test/java/dat3/car/CarApplicationTests.java
+++ src/test/java/dat3/car/CarApplicationTests.java
@@ -1,13 +1,10 @@
 package dat3.car;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
-
 }
